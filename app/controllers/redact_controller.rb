71228f42242fb4933402f0fb3fdac0ca2202f1f7
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-07-11 12:57:53 +0530

    POST method redact added

--- app/controllers/redact_controller.rb
+++ app/controllers/redact_controller.rb
@@ -1,5 +1,14 @@
-class RedactController < ApplicationController
+class RedactController < ApplicationController  
+  protect_from_forgery with: :null_session
+
   def index 
     render json: "Redaction Service", status: :ok 
+  end
+
+  def modify
+    statement = params["_json"]
+    regex = Regexp.new("\\b(?:#{REDACTION_WORDS.join('|')})\\b", Regexp::IGNORECASE)
+    # Replace the matched REDACTION_WORDS with the word 'REDACTED'
+    render json: statement.gsub(regex, "REDACTED")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/redact", to: 'redact#index'
+  post "redact", to: 'redact#modify'
 
   # Defines the root path route ("/")
   # root "posts#index"
